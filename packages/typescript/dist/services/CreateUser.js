e2e07b197e9c2c17c8c30a9297760dd744f9908a
jerp86 <jerp4@hotmail.com>
2020-04-15 20:35:44 -0300

    Definindo o repositório no GitHub

---
+++ packages/typescript/dist/services/CreateUser.js
@@ -0,0 +1,17 @@
+"use strict";
+/**
+ * Para criar um usuário, será necessário informar os seguintes parâmetros:
+ * name, email, password
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+function createUser(_a) {
+    var _b = _a.name, name = _b === void 0 ? '' : _b, email = _a.email, password = _a.password, techs = _a.techs;
+    var user = {
+        name: name,
+        email: email,
+        password: password,
+        techs: techs,
+    };
+    return user;
+}
+exports.default = createUser;
